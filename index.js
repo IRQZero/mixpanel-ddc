2effe6b1e96fa975a3ea4af50485fde1e9672552
root <root@beaglebone.(none)>
2014-07-04 01:24:46 +0000

    intialize the repository

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+ *  each NFC Node is a socket.io client that performs two actions upon receiving NFC Data
+ *  1. backs up NFC data to local mongo instance
+ *  2. emits an event to the central server containing the newest data that has not been confirmed sent
+ */
+
+/* 
+    1. connect to mongodb if not connected
+      1.a. change lights according to state
+    2. connect to central socket.io server if not connected
+      2.a. change lights according to state
+    3. when NFC data is received
+      3.a. store data in mongo FiLo
+      3.b. change lights according to state
+    4. on server "report" event, send oldest unconfirmed NFC record from mongo db
+      4.a. 
+
+ */
+
+var io = require('socket.io')();
+io.sockets.emit('an event sent to all connected clients');
+io.emit('an event sent to all connected clients');
+
+
+
