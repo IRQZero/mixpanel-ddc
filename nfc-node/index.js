4485cf21b345c09462eaf933fcaff29a4fefc94e
Matt Howell <misterinterrupt@gmail.com>
2014-07-27 14:21:34 -0700

    made the index do the writing

--- nfc-node/index.js
+++ nfc-node/index.js
@@ -15,16 +15,38 @@
       3.b. change lights according to state
     4. on server "report" event, send oldest unconfirmed NFC record from mongo db
       4.a. 
-
  */
 
-var io = require('socket.io')();
-var mdns = require('mdns');
+// var io = require('socket.io')();
 
-mdns.createAdvertisement(mdns.udp('ddc') , 7777 ).start();
+// io.sockets.emit('an event sent to all connected clients');
+// io.emit('an event sent to all connected clients');
 
-io.sockets.emit('an event sent to all connected clients');
-io.emit('an event sent to all connected clients');
+// Copyright 2013 Don Coleman
 
+// create a NDEF message with multiple records
 
+var fs = require('fs'),
+    ndef = require('ndef'),
+    message,
+    byteArray,
+    buffer;
 
+message = [ 
+    ndef.uriRecord("http://nodejs.org"),
+    ndef.textRecord("hello, world")
+];
+console.log(message);
+
+byteArray = ndef.encodeMessage(message);
+buffer = new Buffer(byteArray);
+
+console.log(buffer.toString('base64'));
+
+fs.writeFile("multiple_records.mfd", buffer, function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("The file was saved!");
+    }
+}); 
