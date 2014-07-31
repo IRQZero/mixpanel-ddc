ae908dee3dc973d37ea07715ccc271f18d58fd92
Tim Trefren <tim@mixpanel.com>
2014-07-31 16:26:38 -0700

    add sleep back in

--- restart_full.sh
+++ restart_full.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 screen -S reader_server -p 0 -X quit
-export NODE_ENV=production; screen -S reader_server -d -m nice --adjustment=-20 node index.js
+export NODE_ENV=production; screen -S reader_server -d -m nice --adjustment=-20 node index.js; sleep 1
